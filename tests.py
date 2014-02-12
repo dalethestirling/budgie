233cf4b3c9babec3454e2f7831b83802d1d028a7
Dale <dale@puredistortion.com>
2014-02-12 23:36:05 +1100

    working out why tests.py is broken

--- tests.py
+++ tests.py
@@ -9,15 +9,16 @@ class TestCadre(unittest.TestCase):
         '''Test the import of cadre python module'''
         import cadre
 
-    # def test_ssh_call(self):
-    #     '''Test the __call__ that raises the NotImplementedError'''
-    #     import cadre
-    #     with self.assertRaises(NotImplementedError):
-    #         cadre.ssh('localhost')()
+    def test_ssh_call(self):
+        '''Test the __call__ that raises the NotImplementedError'''
+        import cadre
+        with self.assertRaises(NotImplementedError):
+            cadre.ssh('localhost')()
 
     def test_ssh_run_cmd(self):
         '''Test that a command can be executed on the remote host'''
         print os.environ['USER']
+        print tr(cadre.ssh('localhost').whoami()).strip('\n')
         import cadre
         self.assertEqual(str(cadre.ssh('localhost').whoami()).strip('\n'), os.environ['USER'])
 
