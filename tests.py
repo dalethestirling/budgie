8b32c58f4cfd775f7cb1fe0a422d3061da69e8fa
Dale <dale@puredistortion.com>
2014-02-12 23:05:20 +1100

    getting tests to work in travis

--- tests.py
+++ tests.py
@@ -13,12 +13,12 @@ class TestCadre(unittest.TestCase):
         '''Test the __call__ that raises the NotImplementedError'''
         import cadre
         with self.assertRaises(NotImplementedError):
-            cadre.ssh(localhost)()
+            cadre.ssh('localhost')()
 
     def test_ssh_run_cmd(self):
         '''Test that a command can be executed on the remote host'''
         import cadre
-        self.assertEqual(str(cadre.ssh(localhost).whoami()).strip('\n'), os.environ['USER'])
+        self.assertEqual(str(cadre.ssh('localhost').whoami()).strip('\n'), os.environ['USER'])
 
 if __name__ == '__main__':
 
