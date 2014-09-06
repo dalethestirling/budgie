c9218e96e7046b07a7094a7a95d13850d9136e48
Dale <dale@puredistortion.com>
2014-09-06 22:34:54 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='budgie',
+      version='0.1',
+      description='Pythonic remote commands via ssh',
+      author='Dale Stirling and Darren Wurf',
+      author_email='feedback@puredistortion.com',
+      url='http://www.python.org/puredistortion/budgie',
+      packages=['sh', ],
+     )
