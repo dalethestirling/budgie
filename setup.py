16082233698975e745b470e6ce5cb3bbf81b6ac5
Dale <dale@puredistortion.com>
2014-09-07 21:58:21 +1000

    enabled pypi and pip

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='budgie',
       version='0.1',
@@ -8,5 +8,19 @@ setup(name='budgie',
       author='Dale Stirling and Darren Wurf',
       author_email='feedback@puredistortion.com',
       url='http://www.python.org/puredistortion/budgie',
-      packages=['sh', ],
+      py_modules=['budgie'],
+      install_requires=['sh'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
      )
